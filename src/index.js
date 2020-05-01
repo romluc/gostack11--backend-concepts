e3e313d7b1d05c7d87304fabe8cba833f49027a4
romluc <romulofilhofilho@gmail.com>
2020-05-01 11:58:17 -0300

    Create CRUD routes

--- src/index.js
+++ src/index.js
@@ -3,30 +3,66 @@ const { uuid } = require('uuidv4');
 
 const app = express();
 
+// So we can retrieve body request info
+app.use(express.json());
+
+// Hard-coded local endpoint
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.json(['Project 1', 'Project 2']);
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json(['Project 1', 'Project 2', 'Project 3', 'Project 4']);
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Project 1',
-    'Project 2',
-    'Project 3',
-    'Project 4',
-    'Project 5',
-  ]);
+  const { id } = request.params;
+  const { owner, title } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
   const { id } = request.params;
-  console.log(id);
-  return response.json(['Project 1', 'Project 2', 'Project 3', 'Project 4']);
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  // Using splice method to remove 1 item starting from projectIndex
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
